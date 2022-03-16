40447dc457294b846a188a87b1178128fb641475
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-16 13:01:48 +0000

    Question 5 completed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ It goes through num = myFunction(num) again with num being equal to 9, so 9-1 eq
 
 // # Q4
 
-function myFunction(num) {
+/* function myFunction(num) {
   return num - 1;
 }
 
@@ -38,10 +38,29 @@ let add = 3;
 add = myFunction(add);
 add = myFunction(add);
 
-/* The let num = 10 is a global variable.
+/* What is the value of add and num at the end of the program? Why?
+The let num = 10 is a global variable.
 However let num = 10 is never used because the let add = 3 is taking over.
 Let add = 3 is a function scope and when I console.log(add) it gives me 1. 
 It goes through the add = myFunction(add) TWICE. So initially the result is 2, and then 1.
+As a result, the value of add is 1, the value of num is 10.
 
 The result was not what I was expecting. I expected return 10 -1 to equal 9. 
 However, when I console.log(num), it remains 10, nothing changes. */
+
+// # Q5
+
+function myFunction(num, num1) {
+  console.log(num1);
+}
+
+let num = 10;
+let num1 = 2;
+
+myFunction(num);
+
+/* What value will be logged inside the function call? Why?
+The value that is logged inside the function call is 10. 
+This is because the variable num equals 10. The variable num is assigned in one of the myFunction arguments.
+
+Yes, the result is what I've been expecting. */
